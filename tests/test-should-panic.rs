31c8bae7ec456d1472876a46d3a5952797304fb6
Arun Ravi <arunravi728@gmail.com>
2025-12-28 22:51:02 -0800

    Removed harness function from single test case test suites.

--- tests/test-should-panic.rs
+++ tests/test-should-panic.rs
@@ -1,16 +1,14 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-// We use a custom test runner defined in this file as opposed to the one defined in lib.rs.
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "run_test"]
 
 use core::panic::PanicInfo;
 use triad::{QemuExitCode, exit_qemu, serial_println, serial_print};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    run_test();
+    should_panic();
+    serial_println!("[Test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
@@ -21,19 +19,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[Test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn test_should_panic() {
+fn should_panic() {
     serial_print!("test_should_panic...");
     assert_eq!(0, 1);
 }
