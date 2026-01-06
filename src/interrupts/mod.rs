758445d3a70bfe094dd7d66a97f048bf3d920ef6
Arun Ravi <arunravi728@gmail.com>
2026-01-05 20:12:28 -0800

    Improve interrupt stack frame infor by using naked functions.
    The function prologue pollutes the interrupt stack frames. We thus create macro that calls a naked function to invoke the interrupt handler.

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -5,15 +5,41 @@ use crate::interrupts::idt::IdtIndex;
 pub mod idt;
 pub mod privilege;
 
-mod interrupt_handlers;
+#[derive(Debug)]
+#[repr(C)]
+struct ExceptionStackFrame {
+    instruction_pointer: u64,
+    code_segment: u64,
+    cpu_flags: u64,
+    stack_pointer: u64,
+    stack_segment: u64,
+}
+
+// The function prologue is a few lines of code at the beginning of a function, which prepare the
+// stack and registers for use within the function. Thus, the prologue generally tends to write
+// information before we can access the interrupt stack frame. To access the correct addresses,
+// we need to run functions without a prologue. The [naked] attribute helps with this. Note we
+// cannot call anything but a naked_asm! call from a naked function. Hence, link Rust functions
+// to handle specific exceptions.
+macro_rules! handler {
+    ($name: ident) => {{
+        #[unsafe(naked)]
+        extern "C" fn wrapper() -> ! {
+            core::arch::naked_asm!(
+                "mov rdi, rsp",
+                "sub rsp, 8",
+                "call {}",
+                sym $name,
+            );
+        }
+        wrapper
+    }}
+}
 
 lazy_static! {
     static ref IDT: idt::InterruptDescriptorTable = {
         let mut idt = idt::InterruptDescriptorTable::new();
-        idt.add_interrupt_handler(
-            IdtIndex::DivideErrorIndex,
-            interrupt_handlers::divide_error_handler,
-        );
+        idt.add_interrupt_handler(IdtIndex::DivideErrorIndex, handler!(divide_error_handler));
 
         idt
     };
@@ -21,4 +47,9 @@ lazy_static! {
 
 pub fn init() {
     IDT.load();
+}
+
+extern "C" fn divide_error_handler(stack_frame: &ExceptionStackFrame) -> ! {
+    crate::println!("\nEXCEPTION: DIVIDE BY ZERO\n{:#?}", &*stack_frame);
+    loop {}
 }
