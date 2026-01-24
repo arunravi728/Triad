12f3c724d237c116a5a81d40bcd8ccb94346127d
Arun Ravi <arunravi728@gmail.com>
2026-01-23 19:34:00 -0800

    Add functionality to create a two chained NICs

--- src/interrupts/pic.rs
+++ src/interrupts/pic.rs
@@ -6,6 +6,33 @@ use x86_64::instructions::port::Port;
 
 const CMD_EOI: u8 = 0x20;
 
+const PRIMARY_CMD_PORT: u16 = 0x20;
+const PRIMARY_DATA_PORT: u16 = 0x21;
+const SECONDARY_CMD_PORT: u16 = 0xA0;
+const SECONDARY_DATA_PORT: u16 = 0xA1;
+
+pub struct Pics {
+    primary: Pic,
+    secondary: Pic,
+}
+
+impl Pics {
+    pub unsafe fn new(primary_offset: u8, secondary_offset: u8) -> Self {
+        Pics {
+            primary: Pic {
+                idt_base_offset: primary_offset,
+                command_port: Port::new(PRIMARY_CMD_PORT),
+                data_port: Port::new(PRIMARY_DATA_PORT),
+            },
+            secondary: Pic {
+                idt_base_offset: secondary_offset,
+                command_port: Port::new(SECONDARY_CMD_PORT),
+                data_port: Port::new(SECONDARY_DATA_PORT),
+            },
+        }
+    }
+}
+
 struct Pic {
     idt_base_offset: u8,
     command_port: Port<u8>,
