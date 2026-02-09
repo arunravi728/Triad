8d020cf78dbecc334d01a6e3f999a41256ad8cbd
Arun Ravi <arunravi728@gmail.com>
2026-02-08 17:45:26 -0800

    Remove serial support from the logging library

--- kernel/src/print/log.rs
+++ kernel/src/print/log.rs
@@ -1,5 +1,5 @@
 use bootloader_api::info::FrameBufferInfo;
-use bootloader_x86_64_common::{framebuffer::FrameBufferWriter, serial::SerialPort};
+use bootloader_x86_64_common::framebuffer::FrameBufferWriter;
 use conquer_once::spin::OnceCell;
 use core::fmt::Write;
 use spinning_top::Spinlock;
@@ -10,7 +10,6 @@ pub static LOGGER: OnceCell<KernelLogger> = OnceCell::uninit();
 // A logger instance protected by a spinlock.
 pub struct KernelLogger {
     framebuffer: Option<Spinlock<FrameBufferWriter>>,
-    serial: Option<Spinlock<SerialPort>>,
 }
 
 impl KernelLogger {
@@ -18,32 +17,19 @@ impl KernelLogger {
         framebuffer: &'static mut [u8],
         info: FrameBufferInfo,
         frame_buffer_logger_status: bool,
-        serial_logger_status: bool,
     ) -> Self {
         let framebuffer = match frame_buffer_logger_status {
             true => Some(Spinlock::new(FrameBufferWriter::new(framebuffer, info))),
             false => None,
         };
 
-        let serial = match serial_logger_status {
-            true => Some(Spinlock::new(unsafe { SerialPort::init() })),
-            false => None,
-        };
-
-        KernelLogger {
-            framebuffer,
-            serial,
-        }
+        KernelLogger { framebuffer }
     }
 
     pub fn print_raw(&self, args: core::fmt::Arguments) {
         if let Some(framebuffer) = &self.framebuffer {
             let mut framebuffer = framebuffer.lock();
             framebuffer.write_fmt(args).unwrap();
-        }
-        if let Some(serial) = &self.serial {
-            let mut serial = serial.lock();
-            serial.write_fmt(args).unwrap();
         }
     }
 
@@ -54,9 +40,6 @@ impl KernelLogger {
     pub unsafe fn force_unlock(&self) {
         if let Some(framebuffer) = &self.framebuffer {
             unsafe { framebuffer.force_unlock() };
-        }
-        if let Some(serial) = &self.serial {
-            unsafe { serial.force_unlock() };
         }
     }
 }
@@ -89,18 +72,13 @@ impl log::Log for KernelLogger {
             let mut framebuffer = framebuffer.lock();
             writeln!(framebuffer, "{:5}: {}", record.level(), record.args()).unwrap();
         }
-        if let Some(serial) = &self.serial {
-            let mut serial = serial.lock();
-            writeln!(serial, "{:5}: {}", record.level(), record.args()).unwrap();
-        }
     }
 
     fn flush(&self) {}
 }
 
 pub fn init_logger(buffer: &'static mut [u8], info: FrameBufferInfo) {
-    let logger = LOGGER.get_or_init(move || KernelLogger::new(buffer, info, true, false));
+    let logger = LOGGER.get_or_init(move || KernelLogger::new(buffer, info, true));
     log::set_logger(logger).expect("Logger already set");
     log::set_max_level(log::LevelFilter::Trace);
-    log::info!("Hello, Kernel Mode!");
 }
