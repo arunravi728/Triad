dd3e46b9f1162b7ba0731408430c02066df62e85
Arun Ravi <arunravi728@gmail.com>
2026-05-17 13:29:25 -0700

    Add simple function to add Virtual Addresses

--- kernel/src/memory/virtual_address.rs
+++ kernel/src/memory/virtual_address.rs
@@ -1,3 +1,5 @@
+use core::ops::Add;
+
 // A 64 bit virtual address
 //
 // On x86_64 machines, only the lower 48 bits can be used. This is because x86_64 machines only
@@ -6,10 +8,45 @@
 pub struct VirtualAddress(u64);
 
 impl VirtualAddress {
+    #[inline]
     pub fn new(addr: u64) -> VirtualAddress {
         // addr << 16 --> Moves the 48th bit to the MSB
         // Right shifting an i64 leads to two's complement MSB sign extension
         // eg:- 0000x000 << 4 --> x0000000 as i64 >> 4 --> xxxxx000
         VirtualAddress(((addr << 16) as i64 >> 16) as u64)
+    }
+
+    #[inline]
+    pub fn zero() -> VirtualAddress {
+        VirtualAddress(0x00)
+    }
+
+    #[inline]
+    pub fn address(&self) -> u64 {
+        self.0
+    }
+}
+
+impl Add<u64> for VirtualAddress {
+    type Output = VirtualAddress;
+    #[inline]
+    fn add(self, rhs: u64) -> Self::Output {
+        VirtualAddress::new(self.address() + rhs as u64)
+    }
+}
+
+impl Add<usize> for VirtualAddress {
+    type Output = VirtualAddress;
+    #[inline]
+    fn add(self, rhs: usize) -> Self::Output {
+        VirtualAddress::new(self.address() + rhs as u64)
+    }
+}
+
+impl Add<VirtualAddress> for VirtualAddress {
+    type Output = VirtualAddress;
+    #[inline]
+    fn add(self, rhs: VirtualAddress) -> Self::Output {
+        VirtualAddress::new(self.address() + rhs.address())
     }
 }
