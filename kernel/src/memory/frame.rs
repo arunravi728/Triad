6a69e00360f07b1fa605ffe4fac52b312d02bed4
Arun Ravi <arunravi728@gmail.com>
2026-06-18 21:23:24 -0700

    All frames will be of size 4096 bytes

--- kernel/src/memory/frame.rs
+++ kernel/src/memory/frame.rs
@@ -9,23 +9,15 @@ pub const FRAME_SIZE: u64 = 4096;
 #[repr(C)]
 pub struct Frame {
     start_address: PhysicalAddress,
-    size: u64,
 }
 
 impl Frame {
     pub fn new(start_address: PhysicalAddress) -> Frame {
-        Frame {
-            start_address,
-            size: FRAME_SIZE,
-        }
+        Frame { start_address }
     }
 
     pub fn start_address(&self) -> PhysicalAddress {
         self.start_address
-    }
-
-    pub fn size(&self) -> u64 {
-        self.size
     }
 }
 
@@ -92,7 +84,6 @@ fn test_frame_creation_is_successful() {
     let frame = Frame::new(paddr);
 
     assert_eq!(frame.start_address().address(), 0x18);
-    assert_eq!(frame.size(), FRAME_SIZE);
 }
 
 #[test_case]
