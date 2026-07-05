ee8bf37f9301cf8fadba7889d3daeee0e160c388
Arun Ravi <arunravi728@gmail.com>
2026-07-05 10:28:06 -0700

    Increase the size of the logged text

--- kernel/src/print/writer.rs
+++ kernel/src/print/writer.rs
@@ -6,28 +6,28 @@ use noto_sans_mono_bitmap::{
 
 use crate::print::framebuffer::{Color, Position};
 
-/// Additional vertical space between lines
+// Additional vertical space between lines
 const LINE_SPACING: usize = 2;
-/// Additional horizontal space between characters.
+
+// Additional horizontal space between characters.
 const LETTER_SPACING: usize = 0;
 
-/// Padding from the border. Prevent that font is too close to border.
+// Padding from the border. Prevent that font is too close to border.
 const BORDER_PADDING: usize = 1;
 
-/// Backup character if a desired symbol is not available by the font.
-/// The '�' character requires the feature "unicode-specials".
+// Backup character if a desired symbol is not available by the font.
 pub const BACKUP_CHAR: char = '�';
 
-/// Height of each char raster. The font size is ~0.84% of this. Thus, this is the line height that
-/// enables multiple characters to be side-by-side and appear optically in one line in a natural way.
-pub const CHAR_RASTER_HEIGHT: RasterHeight = RasterHeight::Size16;
+// Height of each char raster. The font size is ~0.84% of this. Thus, this is the line height that
+// enables multiple characters to be side-by-side and appear optically in one line in a natural way.
+pub const CHAR_RASTER_HEIGHT: RasterHeight = RasterHeight::Size20;
 
-/// The width of each single symbol of the mono space font.
+// The width of each single symbol of the mono space font.
 pub const CHAR_RASTER_WIDTH: usize = get_raster_width(FontWeight::Regular, CHAR_RASTER_HEIGHT);
 
 pub const FONT_WEIGHT: FontWeight = FontWeight::Regular;
 
-/// Returns the raster of the given char or the raster of [`font_constants::BACKUP_CHAR`].
+// Returns the raster of the given char or the raster of [`font_constants::BACKUP_CHAR`].
 fn get_char_raster(c: char) -> RasterizedChar {
     fn get(c: char) -> Option<RasterizedChar> {
         get_raster(c, FONT_WEIGHT, CHAR_RASTER_HEIGHT)
